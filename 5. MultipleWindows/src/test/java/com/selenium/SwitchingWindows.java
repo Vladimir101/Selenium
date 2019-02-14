02fd428695fef73dd1650a2aa951149063bb6964
Vladimir101 <vladimir100@hotmail.com>
2019-02-13 21:19:33 -0800

    Multiple windows

---
+++ 5. MultipleWindows/src/test/java/com/selenium/SwitchingWindows.java
@@ -0,0 +1,56 @@
+package com.selenium;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SwitchingWindows
+{
+	private WebDriver driver;
+
+	@Before
+	public void setUp() 
+	{
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/windows");
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void switchingWindows()
+	{
+		String firstWindow = driver.getWindowHandle();
+		String secondWindow = "";
+		
+		driver.findElement(By.linkText("Click Here")).click();
+
+// find the second window handle		
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String window: allWindows)
+			if (!window.equals(firstWindow))
+				secondWindow = window;
+		
+// switch to the first window
+		driver.switchTo().window(firstWindow);
+		assertThat(driver.getTitle(), is(equalTo("The Internet")));
+		
+// switch to the second windows
+		driver.switchTo().window(secondWindow);
+		assertThat(driver.getTitle(), is(equalTo("New Window")));
+	}
+	
+	@After
+	public void tearDown()
+	{
+		driver.quit();
+	}
+}
