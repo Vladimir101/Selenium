5213b49b4ba39d8d203d1bd1bdeaeb299cc0be9d
Vladimir101 <vladimir100@hotmail.com>
2019-02-15 00:12:47 -0800

    RemoteWebDriver

---
+++ 7. RemoteWebDriverAndGrid/src/test/java/com/selenium/WorkingWithRemoteWebDriver.java
@@ -0,0 +1,35 @@
+package com.selenium;
+
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class WorkingWithRemoteWebDriver
+{
+	private RemoteWebDriver driver;
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setBrowserName("chrome");
+		driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), capabilities);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void test()
+	{
+		driver.get("http://book.theautomatedtester.co.uk/");
+	}
+	
+	@After
+	public void tearDown()
+	{
+		driver.quit();
+	}
+}
