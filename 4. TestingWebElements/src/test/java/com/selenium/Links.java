3f540d1e6793807ba3141fa4a6470d4b6c249c21
Vladimir101 <vladimir100@hotmail.com>
2019-03-02 15:12:23 -0800

    refactor

--- 4. TestingWebElements/src/test/java/com/selenium/Links.java
+++ 4. TestingWebElements/src/test/java/com/selenium/Links.java
@@ -5,30 +5,20 @@ import static org.junit.Assert.*;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Links
 {
 	private static WebDriver driver;
 
-	@BeforeClass
-	public static void oneTimeSetUp()
+	@Before
+	public void setUp()
 	{
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-	}
-
-	@Before
-	public void setUp()
-	{
 		driver.get("http://book.theautomatedtester.co.uk/");
 		driver.manage().window().maximize();
 	}
@@ -51,7 +41,7 @@ public class Links
 	}
 	
 	@Test
-	public void getHTMLElement()
+	public void getPageHTML()
 	{
 		String body = driver.getPageSource();
 		System.out.println("Page HTML code:");
@@ -59,8 +49,8 @@ public class Links
 		assertTrue(body.contains("Selenium"));
 	}
 	
-	@AfterClass
-	public static void oneTimeTearDown()
+	@After
+	public void tearDown()
 	{
 		driver.quit();
 	}
