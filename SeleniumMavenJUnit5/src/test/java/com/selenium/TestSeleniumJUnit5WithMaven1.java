daa5d511da718737dbb4cd23342e44e21f95aa60
Vladimir101 <vladimir100@hotmail.com>
2019-05-03 16:32:54 -0700

    Run Selenium on Jenkins

---
+++ SeleniumMavenJUnit5/src/test/java/com/selenium/TestSeleniumJUnit5WithMaven1.java
@@ -0,0 +1,33 @@
+package com.selenium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class TestSeleniumJUnit5WithMaven1
+{
+	private WebDriver driver;
+
+	@BeforeEach
+	void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	void openSchoolWebsite()
+	{
+		System.out.println("Hi, School!");
+		driver.get("https://www.portnov.com");
+		System.out.println("Title of the page: " + driver.getTitle());
+	}
+	
+	@AfterEach
+	void tearDown()
+	{
+		driver.quit();
+	}
+}
---
+++ SeleniumMavenJUnit5/src/test/java/com/selenium/TestSeleniumJUnit5WithMaven2.java
@@ -0,0 +1,33 @@
+package com.selenium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class TestSeleniumJUnit5WithMaven2
+{
+	private WebDriver driver;
+
+	@BeforeEach
+	void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	void openSeleniumWebsite()
+	{
+		System.out.println("Hi, Selenium!");
+		driver.get("https://www.seleniumhq.org/");
+		System.out.println("Title of the page: " + driver.getTitle());
+	}
+	
+	@AfterEach
+	void tearDown()
+	{
+		driver.quit();
+	}
+}
