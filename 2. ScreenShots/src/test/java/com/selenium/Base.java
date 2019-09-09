7436b8a57d3f885638daa514c954fcb8a2f29397
Vladimir101 <vladimir100@hotmail.com>
2019-09-08 18:49:54 -0700

    minor

--- 2. ScreenShots/src/test/java/com/selenium/Base.java
+++ 2. ScreenShots/src/test/java/com/selenium/Base.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
