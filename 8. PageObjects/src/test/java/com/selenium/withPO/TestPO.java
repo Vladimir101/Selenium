602b8eb2dc42c681032bbb2e0dc33f598d8ea068
Vladimir101 <vladimir100@hotmail.com>
2019-09-26 15:44:19 -0700

    added invalid login

--- 8. PageObjects/src/test/java/com/selenium/withPO/TestPO.java
+++ 8. PageObjects/src/test/java/com/selenium/withPO/TestPO.java
@@ -2,9 +2,11 @@ package com.selenium.withPO;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,22 +14,22 @@ public class TestPO
 {
 	private WebDriver driver;
 	
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception
 	{
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+//		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
 
-	@After
+	@AfterEach
 	public void tearDown() throws Exception
 	{
 		driver.quit();
 	}
 
 	@Test
-	public void testValidLogin()
+	public void validLogin()
 	{
 		HomePage homePage = LoginPage.open(driver)
 				.validLogin("tomsmith", "SuperSecretPassword!");
@@ -36,6 +38,13 @@ public class TestPO
 		LoginPage loginPage = homePage.logout();
 		assertTrue(loginPage.getConfirmation().contains("You logged out"));
 	}
-
-// create test for invalid login	
+	 
+	@ParameterizedTest
+	@CsvFileSource(resources = "ddt.csv", numLinesToSkip = 1)
+	void invalidLogin(String username, String password, String errorMessage) 
+	{
+		LoginPage loginPage = LoginPage.open(driver);
+		loginPage.submitLogin(username, password);
+		assertTrue(loginPage.getConfirmation().contains(errorMessage));
+	}
 }
