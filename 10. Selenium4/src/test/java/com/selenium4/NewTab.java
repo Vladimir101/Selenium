a6045369c09dbb4a498b1d2e1b83d232b3d942e9
Vladimir101 <vladimir100@hotmail.com>
2019-11-09 18:34:59 -0800

    Selenium 4 new features

---
+++ 10. Selenium4/src/test/java/com/selenium4/NewTab.java
@@ -0,0 +1,46 @@
+package com.selenium4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class NewTab
+{
+	private WebDriver driver;
+	private String URL = "https://www.seleniumhq.org/";
+
+	@BeforeAll
+	static void setUpBeforeClass()
+	{
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	void setUp()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(URL);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception
+	{
+		driver.quit();
+	}
+
+	@Test
+	public void openNewTab()
+	{
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.navigate().to("https://www.google.com");
+	}
+}
---
+++ 10. Selenium4/src/test/java/com/selenium4/Screenshot.java
@@ -0,0 +1,47 @@
+package com.selenium4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class Screenshot
+{
+	private WebDriver driver;
+	private String URL = "https://www.seleniumhq.org/";
+
+	@BeforeAll
+	static void setUpBeforeClass()
+	{
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	void setUp()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(URL);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception
+	{
+		driver.quit();
+	}
+
+	@Test
+	public void screenshotOfWebElement() throws IOException
+	{
+		WebElement logo = driver.findElement(By.xpath("//div[@id='sidebar']/img[1]"));
+		File file = logo.getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(file, new File("logo.png"));
+	}
+}
