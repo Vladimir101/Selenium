10063cb0960080c16b037bcb11dc9ea5130c7f1c
Vladimir101 <vladimir100@hotmail.com>
2020-03-09 12:34:15 -0700

    Added drag and drop tests

--- 6. WorkingWithActionsAndAlert/src/test/java/com/selenium/WorkingWithActions.java
+++ 6. WorkingWithActionsAndAlert/src/test/java/com/selenium/WorkingWithActions.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class WorkingWithActions
 {
 	private WebDriver driver;
@@ -21,7 +23,7 @@ public class WorkingWithActions
 	@Before
 	public void setUp() 
 	{
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		builder = new Actions(driver);
@@ -57,9 +59,40 @@ public class WorkingWithActions
 		assertEquals("Join", driver.findElement(By.xpath("//h2[1]")).getText());
 	}
 	
+	@Test
+	public void dragAndDrop()
+	{
+		driver.get("https://demoqa.com/droppable/");
+		WebElement source = driver.findElement(By.id("draggable"));
+		WebElement target = driver.findElement(By.id("droppable"));
+		builder.dragAndDrop(source, target).perform();
+		
+		assertEquals("Dropped!", target.getText());
+	}
+	
+	@Test
+	public void dragByOffset()
+	{
+		driver.get("https://demoqa.com/droppable/");
+		WebElement source = driver.findElement(By.id("draggable"));
+		WebElement target = driver.findElement(By.id("droppable"));
+		
+		int xSource = source.getLocation().getX();	 
+		int ySource = source.getLocation().getY();
+		
+		int xTarget = target.getLocation().getX();	 
+		int yTarget = target.getLocation().getY();
+		
+		int xMove = xTarget - xSource + 11;
+		int yMove = yTarget - ySource + 11;
+
+		builder.dragAndDropBy(source, xMove, yMove).perform();
+		assertEquals("Dropped!", target.getText());		
+	}
+	
 	@After
 	public void tearDown()
 	{
-		driver.quit();
+//		driver.quit();
 	}
 }
