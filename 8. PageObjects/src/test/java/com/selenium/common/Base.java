65a97159d43af726077f8fa9fea8dc9a2817637e
Vladimir101 <vladimir100@hotmail.com>
2020-03-10 17:40:11 -0700

    more stable operations

--- 8. PageObjects/src/test/java/com/selenium/common/Base.java
+++ 8. PageObjects/src/test/java/com/selenium/common/Base.java
@@ -15,7 +15,7 @@ public class Base
 {
 	protected WebDriver driver = null;
 	
-	// using FluentWait
+// using FluentWait
 	protected void waitForElementToBeClickable(WebElement element)
 	{
 		new FluentWait<WebDriver>(driver)
@@ -25,18 +25,35 @@ public class Base
 			.ignoring(StaleElementReferenceException.class)
 			.until(ExpectedConditions.elementToBeClickable(element));
 	}
+	
+	protected WebElement waitForElementToBeClickable(By locator)
+	{
+		return new FluentWait<WebDriver>(driver)
+			.withTimeout(Duration.ofSeconds(30))
+			.pollingEvery(Duration.ofSeconds(2))
+			.ignoring(NoSuchElementException.class)
+			.ignoring(StaleElementReferenceException.class)
+			.until(ExpectedConditions.elementToBeClickable(locator));
+	}
 
+// stable operations	
 	protected void clickElement(WebElement element)
 	{
 		waitForElementToBeClickable(element);
 		element.click();
 	}
 	
-	protected void clickElement(By element)
+	protected void clickElementWithFluentWait(By locator)
+	{
+		WebElement element = waitForElementToBeClickable(locator);
+		element.click();
+	}
+	
+	protected void clickElement(By locator)
 	{
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 		wait.until(ExpectedConditions
-				            .elementToBeClickable(element))
+				            .elementToBeClickable(locator))
 							.click();
 	}
 }
