6c2c5724d0bceceb5812030c0952cd2c3b80d210
Vladimir101 <vladimir100@hotmail.com>
2020-03-10 17:44:15 -0700

    minor

--- 8. PageObjects/src/test/java/com/selenium/common/Base.java
+++ 8. PageObjects/src/test/java/com/selenium/common/Base.java
@@ -1,15 +1,8 @@
 package com.selenium.common;
 
 import java.time.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
 
 public class Base
 {
