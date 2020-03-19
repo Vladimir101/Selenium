6402d340629465219912c676d085a5561f3acad7
Vladimir101 <vladimir100@hotmail.com>
2020-03-19 12:55:58 -0700

    minor

--- 9. SeleniumMavenJUnit5/src/test/java/com/selenium/TestSeleniumJUnit5WithMaven1.java
+++ 9. SeleniumMavenJUnit5/src/test/java/com/selenium/TestSeleniumJUnit5WithMaven1.java
@@ -1,19 +1,30 @@
 package com.selenium;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 // starts with Test*** for Surefire plugin convention
 class TestSeleniumJUnit5WithMaven1
 {
 	private WebDriver driver;
 
+	@BeforeAll
+	static void oneTimeSetUp()
+	{
+		WebDriverManager.chromedriver().setup();
+	}
+	
 	@BeforeEach
 	void setUp()
 	{
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 	}
 
@@ -21,8 +32,10 @@ class TestSeleniumJUnit5WithMaven1
 	void openSchoolWebsite()
 	{
 		System.out.println("Hi, School!");
-		driver.get("https://pasv.us/");
-		System.out.println("Title of the page: " + driver.getTitle());
+		driver.get("https://pasv.us/en/");
+		String text = driver.findElement(By.cssSelector(".t480__title.t-title.t-title_xs.t-margin_auto"))
+				.getText();
+		assertEquals("We teach programming\nand QA", text);
 	}
 	
 	@AfterEach
--- 9. SeleniumMavenJUnit5/src/test/java/com/selenium/TestSeleniumJUnit5WithMaven2.java
+++ 9. SeleniumMavenJUnit5/src/test/java/com/selenium/TestSeleniumJUnit5WithMaven2.java
@@ -1,22 +1,30 @@
 package com.selenium;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 //starts with Test*** for Surefire plugin convention
 class TestSeleniumJUnit5WithMaven2
 {
 	private WebDriver driver;
 
+	@BeforeAll
+	static void oneTimeSetUp()
+	{
+		WebDriverManager.chromedriver().setup();
+	}
+	
 	@BeforeEach
 	void setUp()
 	{
 		String name = System.getProperty("os.name");
 		System.out.println("OS name is " + name);
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 	}
 
