c0d04914034568b5a288aa1b1e324fe1d48d1ad6
Vladimir101 <vladimir100@hotmail.com>
2020-05-28 18:52:19 -0700

    added isLoaded()

--- 8. PageObjects/src/test/java/com/selenium/withPO/LoginPage.java
+++ 8. PageObjects/src/test/java/com/selenium/withPO/LoginPage.java
@@ -2,10 +2,13 @@ package com.selenium.withPO;
 
 import static org.junit.Assert.assertEquals;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.selenium.common.Base;
 
@@ -31,8 +34,10 @@ public class LoginPage extends Base
 	public LoginPage(WebDriver driver)
 	{
 		this.driver = driver;
-		assertEquals("This is not the login page", 
-				title, driver.getTitle());
+		/*
+		 * assertEquals("This is not the login page",
+		 * title, driver.getTitle());
+		 */
 	}
 	
 // services
@@ -65,4 +70,11 @@ public class LoginPage extends Base
 	{
 		return confirmation.getText();
 	}
+	
+	public boolean isLoaded() 
+	{
+        return new WebDriverWait(driver, 10)
+        		.until(ExpectedConditions.presenceOfElementLocated(By.id("some element")))
+        		.isDisplayed();
+    }
 }
