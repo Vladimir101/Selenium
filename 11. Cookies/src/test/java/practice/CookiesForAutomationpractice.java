654bd358b69af9f1868bf395b4e1a22f8e2234f5
Vladimir101 <vladimirbelor100@gmail.com>
2021-05-17 20:47:31 -0700

    login with cookies

---
+++ 11. Cookies/src/test/java/practice/CookiesForAutomationpractice.java
@@ -0,0 +1,58 @@
+package practice;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CookiesForAutomationpractice
+{
+	private WebDriver driver;
+	static Set<Cookie> cookies;
+	
+	@BeforeAll
+	static void oneTimeSetUp()
+	{
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception
+	{
+		driver = new ChromeDriver();
+		driver.get("http://automationpractice.com/index.php?controller=authentication");
+		driver.manage().window().maximize();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception
+	{
+		driver.quit();
+	}
+	
+	@Order(1)
+	@Test
+	void getCookies()
+	{
+		driver.findElement(By.id("email")).sendKeys("realsqa@gmail.com");
+		driver.findElement(By.id("passwd")).sendKeys("abc123");
+		driver.findElement(By.id("SubmitLogin")).click();
+		
+		cookies = driver.manage().getCookies();
+	}
+	
+	@Test
+	void loginWithCookies() throws InterruptedException
+	{
+		for (Cookie cookie : cookies)
+			driver.manage().addCookie(cookie);
+
+// http://automationpractice.com/index.php?controller=authentication	
+		driver.get("http://automationpractice.com/index.php?id_category=3&controller=category");	
+		Thread.sleep(5000);
+	}
+}
